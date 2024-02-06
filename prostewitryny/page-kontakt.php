61ecd1cf78bc81011deb8941c0e21674caee3fe9
Bartosz Domagalski <bdomagalski88@gmail.com>
2024-02-06 19:27:26 +0100

    zmiana danych firmy i usunięcie siedziby w słomnikach

--- prostewitryny/page-kontakt.php
+++ prostewitryny/page-kontakt.php
@@ -73,19 +73,6 @@ get_header(); ?>
             </div>
           </div>
         </div>
-        <div class="contact-icon-box">
-          <div class="row no-gutters">
-            <div class="col-2 icon-col">
-              <i class="fas fa-map-marked-alt"></i>
-            </div>
-            <div class="col-10">
-              <h2 class="contact-icon-title">Adres - Siedziba 2</h2>
-              <h3 class="contact-icon-content">
-                  <a href="https://maps.app.goo.gl/vppQrgna74ViDwiC8" target="_blank">Krakowska 62d, 32-090 Słomniki</a>
-              </h3>
-            </div>
-          </div>
-        </div>
 
         <p class="contact-info-godziny">
           Biuro Obsługi Klienta czynne jest w godzinach:<br>
@@ -95,7 +82,7 @@ get_header(); ?>
 
         <p class="contact-info-firma">
           <strong>ProsteWitryny.pl - Twoja Prosta Strona Internetowa Kraków</strong><br>
-          ProsteWitryny.pl jest częścią Family Party non-profit spółka z ograniczoną odpowiedzialnością z siedzibą w Słomnikach przy ulicy Krakowskiej 62d, 32-090 Słomniki, wpisana do rejestru przedsiębiorców przez Sąd Rejonowy dla Krakowa-Śródmieścia w Krakowie, XII Wydział Gospodarczy pod numerem KRS 0000746071, posiadająca REGON 38110158500000 oraz NIP 6821777738
+          ProsteWitryny.pl jest częścią FAMILY PARTY SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ z siedzibą w Słomnikach przy ulicy Krakowskiej 62d, 32-090 Słomniki, wpisana do rejestru przedsiębiorców przez Sąd Rejonowy dla Krakowa-Śródmieścia w Krakowie, XII Wydział Gospodarczy pod numerem KRS 0000746071, posiadająca REGON 38110158500000 oraz NIP 6821777738
         </p>
 
       </div>
@@ -113,20 +100,6 @@ get_header(); ?>
 
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1280.1554587995577!2d19.959893339014677!3d50.08046541408531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b800dd8a60f%3A0x845faefaac08b0b7!2sProsteWitryny.pl!5e0!3m2!1spl!2spl!4v1693345902385!5m2!1spl!2spl" width="1920" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-      </div>
-    </div>
-  </div>
-</section>
-
-<section id="map-siedziba-2">
-
-<div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <h3 class="column-heading">Mapa <span>Siedziba 2 - Słomniki</span></h3>
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17169.848563263626!2d20.05502418714984!3d50.2300523738292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716533170e9360b%3A0xe682d9985bb1a95f!2sProsteWitryny.pl%20-%20Strony%20Internetowe%20WWW%20S%C5%82omniki!5e0!3m2!1spl!2spl!4v1699288843363!5m2!1spl!2spl" width="1920" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          
       </div>
     </div>
   </div>
